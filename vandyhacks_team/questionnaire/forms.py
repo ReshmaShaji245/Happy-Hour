b81ba02b5e846aae7a8cdabb77b0f6c123936d69
mchan63 <mchan63@gatech.edu>
2020-07-11 20:45:17 -0400

    added interest field

--- vandyhacks_team/questionnaire/forms.py
+++ vandyhacks_team/questionnaire/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class InterestForm(forms.Form):
+    interests=forms.CharField()
     hobbies = forms.CharField()
     genres = forms.CharField()
-    material =forms.ChoiceField(choices=[('manga','Manga'),('music','Music')])
+    material =forms.ChoiceField(choices=[('anime','Anime'),('music','Music')])
--- vandyhacks_team/questionnaire/views.py
+++ vandyhacks_team/questionnaire/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import InterestForm
+import http.client
 # Create your views here.
 def contact(request):
 
     if request.method=="POST":
         form=InterestForm(request.POST)
         if form.is_valid():
+            interests=form.cleaned_data['interests']
             hobbies=form.cleaned_data['hobbies']
             genres=form.cleaned_data['genres']
             material=form.cleaned_data['material']
-            print(hobbies,genres,material)
+            print(interests,hobbies,genres,material)
+            conn = http.client.HTTPSConnection("hummingbirdv1.p.rapidapi.com")
+
+            headers = {
+                'x-rapidapi-host': "hummingbirdv1.p.rapidapi.com",
+                'x-rapidapi-key': "9d98761b39msh902e0f78cc831e3p165f1cjsnae93d1bdacdf"
+                }
+
+            conn.request("GET", "/anime/steins-gate", headers=headers)
+
+            res = conn.getresponse()
+            data = res.read()
+
+            print(data.decode("utf-8"))
 
 
     form=InterestForm()
