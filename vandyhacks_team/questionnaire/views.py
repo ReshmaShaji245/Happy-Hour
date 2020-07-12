9e196716026f64743cc92eeffeaae31d5c16039a
mchan63 <mchan63@gatech.edu>
2020-07-11 22:18:15 -0400

    We now have an anime finder

--- vandyhacks_team/questionnaire/views.py
+++ vandyhacks_team/questionnaire/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import InterestForm
 import http.client
+import json
+from datetime import date
 # Create your views here.
 def contact(request):
-
+    today = date.today()
     if request.method=="POST":
         form=InterestForm(request.POST)
         if form.is_valid():
@@ -13,20 +15,26 @@ def contact(request):
             genres=form.cleaned_data['genres']
             material=form.cleaned_data['material']
             print(interests,hobbies,genres,material)
-            conn = http.client.HTTPSConnection("hummingbirdv1.p.rapidapi.com")
+
+            conn = http.client.HTTPSConnection("jikan1.p.rapidapi.com")
 
             headers = {
-                'x-rapidapi-host': "hummingbirdv1.p.rapidapi.com",
+                'x-rapidapi-host': "jikan1.p.rapidapi.com",
                 'x-rapidapi-key': "9d98761b39msh902e0f78cc831e3p165f1cjsnae93d1bdacdf"
                 }
 
-            conn.request("GET", "/anime/steins-gate", headers=headers)
+            conn.request("GET", "/season/"+str(today.year)+"/summer", headers=headers)
 
             res = conn.getresponse()
             data = res.read()
+            y=json.loads(data.decode("utf-8"))
+            list=[]
+            for x in y["anime"]:
+                for z in x["genres"]:
+                    if(z["name"]==genres):
+                        list.append(x["title"])
 
-            print(data.decode("utf-8"))
 
 
     form=InterestForm()
-    return render(request,'form.html',{'form':form})
+    return render(request,'form.html',{'form':form,'list':list})
